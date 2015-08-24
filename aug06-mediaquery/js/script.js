a18baa93aa7112f6ec6736ddea5212f71ece848e
vinferno <vinsonfernandez@gmail.com>
2015-08-24 09:13:22 -0700

    fixing

---
+++ aug06-mediaquery/js/script.js
@@ -0,0 +1,4 @@
+/*put your javascript here
+
+alert("this is a javscript")
+I just came to the starbucks again. so i just got your message but yeah i feel you. it sucks that we cant even be friends cause of her but i understand. you've been through the same thing so you know how it is when someone wont let you even be cool with people as cool as you but thats why i liked talking to you, cause you got me. so super sad. */
